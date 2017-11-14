3b0a8ddcd81777d2020c838cd31418c9e2d135b2
Ksenia Belikova <ksenia.belikova@outlook.com>
2017-11-13 22:53:56 -0500

    add manifest, icon and index.html

--- cas-project/src/index.js
+++ cas-project/src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/index.css';
-import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
+import {render} from 'react-dom';
+import configureStore from './store/configureStore';
+import {Provider} from 'react-redux';
+import {Router, browserHistory} from 'react-router';
+import routes from './routes';
+import './styles/styles.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = configureStore();
+
+render(
+    <Provider store={store}>
+        <Router history={browserHistory} routes={routes}/>
+    </Provider>,
+    document.getElementById('app')
+);
