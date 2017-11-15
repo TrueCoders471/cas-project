48b6ab40e99bc732cd544050ae06ff5893db6f92
Ksenia Belikova <ksenia.belikova@outlook.com>
2017-11-15 13:21:49 -0500

    update login to navigate to admin, student, notetaker or faculty landing pages or display alert with an error

--- cas-project/src/components/loginPage/LoginPage.js
+++ cas-project/src/components/loginPage/LoginPage.js
@@ -4,7 +4,23 @@ class LoginPage extends React.Component {
 
 
     login() {
-        location.href = "/admin";
+        switch (document.getElementById("inputUserName").value) {
+            case 'admin@radford.edu' :
+                location.href = "/admin";
+                break;
+            case 'student@radford.edu' :
+                location.href = "/student";
+                break;
+            case  'notetaker@radford.edu' :
+                location.href = "/notetaker";
+                break;
+            case 'faculty@radford.edu':
+                location.href = "/faculty";
+                break;
+            default :
+                alert("Wrong Username. Try again");
+        }
+
     }
 
     forgotPassword() {
@@ -16,8 +32,8 @@ class LoginPage extends React.Component {
             <div className="container">
                 <form className="form-signin">
                     <h2 className="form-signin-header">Center for Accessability Services</h2>
-                    <label htmlFor="inputUsername" className="sr-only">Username</label>
-                    <input type="text" id="inputUserName" className="form-control" placeholder="Username" required
+                    <label htmlFor="inputUsername" className="sr-only">RU Email</label>
+                    <input type="text" id="inputUserName" className="form-control" placeholder="RU Email" required
                            autoFocus/>
                     <label htmlFor="inoutPassword" className="sr-only">Password</label>
                     <input type="password" id="inputPassword" className="form-control" placeholder="Password" required/>
@@ -26,8 +42,9 @@ class LoginPage extends React.Component {
                             <input type="checkbox" value="remember-me"/>
                             Remember me</label>
                         <button className="btn btn-lg btn-primary" type="submit" onClick={this.login}>Sign in</button>
-                        <br /> <br/>
-                        <button className="btn btn-lg btn-primary" onClick={this.forgotPassword}>Forgot password</button>
+                        <br/> <br/>
+                        <button className="btn btn-lg btn-primary" onClick={this.forgotPassword}>Forgot password
+                        </button>
                     </div>
                 </form>
             </div>
