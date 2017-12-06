d36e48695091ab855b5b7a7b6f39112f03e8032c
Isaac Hutchison <hutchinsongalax@gmail.com>
2017-12-05 22:57:40 -0500

    Added classes view for note takers classes. *Still needs work*

--- cas-project/src/components/classPages/NotetakerClassPage.js
+++ cas-project/src/components/classPages/NotetakerClassPage.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
+
 class NotetakerClassPage extends React.Component {
     render() {
         return (
 
             <div>
                 <p>Info about this class: Notes etc</p>
-
-               
             </div>
+
         );
 
     }
--- cas-project/src/components/notetakerPages/NotetakerClassesPage.js
+++ cas-project/src/components/notetakerPages/NotetakerClassesPage.js
@@ -1,12 +1,57 @@
 import React from 'react';
+import '../notetakerPages/NTCPStyleSheet.css';
 
 class MyClassesNotetakerPage extends React.Component {
     openClass() {
         location.href = "/NotetakerClass";
     }
+
     render() {
         return (
             <div>
+                <div className="section group">
+                    <div className="col span_1_of_5">
+                        <div className="card">
+                            <div className="container">
+
+                                <h4><b>ITEC 120</b></h4>
+                                <p>Principles of Computer Science I</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col span_1_of_5">
+                        <div className="card">
+                            <div className="container">
+                                <h4><b>ITEC 110</b></h4>
+                                <p>Information Technology</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col span_1_of_5">
+                        <div className="card">
+                            <div className="container">
+                                <h4><b>ITEC 110</b></h4>
+                                <p>Information Technology</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col span_1_of_5">
+                        <div className="card">
+                            <div className="container">
+                                <h4><b>ITEC 110</b></h4>
+                                <p>Information Technology</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col span_1_of_5">
+                        <div className="card">
+                            <div className="container">
+                                <h4><b>ITEC 110</b></h4>
+                                <p>Information Technology</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <p>My Classes Notetaker: A grid of classes her with a button for each</p>
                 <button className="btn btn-lg btn-primary" type="submit" onClick={this.openClass}>View Class</button>
             </div>
