b16dafd47f47ba852ca36eb6f1249ff83f8eb234
Ksenia Belikova <ksenia.belikova@outlook.com>
2017-12-06 23:04:07 -0500

    update login (works with username and matching password now)

--- cas-project/src/components/loginPage/LoginPage.js
+++ cas-project/src/components/loginPage/LoginPage.js
@@ -6,35 +6,48 @@ import './loginPage-Styles.css';
  */
 
 class LoginPage extends React.Component {
+    
+    drawLogBackground() {
+        return (<div id="loginBackgroundStretch" src>
+            <img className="background-image" src={require('./../../../public/images/loginBackground.jpg')}/>
+        </div>);
+    }
 
     /**
      * transitions the user to a particular Log-in persona specific view
      * based upon there account type
      */
-    login() {
-        switch (document.getElementById("inputUserName").value) {
-            case 'admin@radford.edu' :
-                location.href = "/admin";
-                break;
-            case 'student@radford.edu' :
-                location.href = "/student";
-                break;
-            case  'notetaker@radford.edu' :
-                location.href = "/notetaker";
-                break;
-            case 'faculty@radford.edu':
-                location.href = "/faculty";
-                break;
-            default :
-                alert("Wrong Username or Password.");
+    submit(e) {
+        e.preventDefault();
+        const admin = document.getElementById("inputUserName").value === 'admin@radford.edu'; // User validate
+        const adminPassword = document.getElementById("inputPassword").value === 'admin';
+
+        const student = document.getElementById("inputUserName").value === 'student@radford.edu'; // User validate
+        const studentPassword = document.getElementById("inputPassword").value === 'student';
+
+        const notetaker = document.getElementById("inputUserName").value === 'notetaker@radford.edu'; // User validate
+        const notetakerPassword = document.getElementById("inputPassword").value === 'notetaker';
+
+        const faculty = document.getElementById("inputUserName").value === 'faculty@radford.edu'; // User validate
+        const facultyPassword = document.getElementById("inputPassword").value === 'faculty';
+
+
+        if (admin === true && adminPassword === true) {
+            location.href = "/admin";
+
+        } else if (student === true && studentPassword === true) {
+            location.href = "/student";
+
+        } else if (notetaker === true && notetakerPassword === true) {
+            location.href = "/notetaker";
+
+        } else if (faculty === true && facultyPassword === true) {
+            location.href = "/faculty";
+
         }
-
-    }
-
-    drawLogBackground() {
-        return(<div id="loginBackgroundStretch" src>
-            <img className = "background-image" src = {require('./../../../public/images/loginBackground.jpg')}/>
-        </div>);
+        else {
+            alert("Wrong Username or Password.");
+        }
     }
 
     /**
@@ -45,22 +58,22 @@ class LoginPage extends React.Component {
         return (
             <div id="LoginView-Container" className="login-page">
                 {this.drawLogBackground()}
-            <div id="loginFields-container">
-                    <form className="form-signin">
+                <div id="loginFields-container">
+                    <form className="form-signin" onSubmit={this.submit}>
                         <div id="top-div-Container">
                             <h2 className="form-signin-header">LOGIN</h2>
                         </div>
                         <br/>
                         <label htmlFor="inputUsername" className="sr-only">RU Email</label>
-                        <input type="text" id="inputUserName" className="form-control" placeholder="RU Email" required
-                           autoFocus />
+                        <input type="text" id="inputUserName" className="form-control" required
+                               placeholder="RU Email" autoFocus/>
                         <br/>
                         <label htmlFor="inoutPassword" className="sr-only">Password</label>
-                        <input type="password" id="inputPassword" className="form-control"
-                                placeholder="Password" required/>
+                        <input type="password" id="inputPassword" className="form-control" required
+                               placeholder="Password"/>
                         <br/>
                         <br/>
-                        <button className="btn btn-lg btn-primary" type="submit" onClick={this.login}>Sign in</button>
+                        <button className="btn btn-lg btn-primary" type="submit">Sign in</button>
                         <div className="checkbox">
                             <label>
 
@@ -76,7 +89,7 @@ class LoginPage extends React.Component {
 
                         </div>
                     </form>
-            </div>
+                </div>
             </div>
         );
     }
